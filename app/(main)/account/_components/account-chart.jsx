5eaff0d8780f1bb9f743ed1fa9c4bacac6cfd45b
Aniket Agrawal <aniket.agra418@gmail.com>
2025-10-20 19:57:54 +0530

    account section completed with bar chart and filter

--- app/(main)/account/_components/account-chart.jsx
+++ app/(main)/account/_components/account-chart.jsx
@@ -1,7 +1,10 @@
 "use client"
 import { endOfDay, format, startOfDay, subDays } from 'date-fns';
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import {  Card,  CardContent,  CardHeader,  CardTitle,} from "@/components/ui/card"
+import {  Select,  SelectContent,  SelectItem,  SelectTrigger,  SelectValue,} from "@/components/ui/select"
+
 
 const DATE_RANGES = {
     "7D": {label: "Last 7 Days", days:7},
@@ -25,7 +28,7 @@ const AccountChart = ({transactions}) => {
 
         //filter transactions within date range
         const filtered = transactions.filter(
-            (t) => new Date(t.date) >= startDate && new DAte(t.date) <= endOfDay(now)
+            (t) => new Date(t.date) >= startDate && new Date(t.date) <= endOfDay(now)
         );
 
         const group = filtered.reduce((acc, transaction) => {
@@ -34,34 +37,136 @@ const AccountChart = ({transactions}) => {
             if(!acc[date]) {
                 acc[date] = {date, income:0 , expense:0}
             }
+
+            if(transaction.type === "INCOME") {
+              acc[date].income += transaction.amount;
+            } else {
+              acc[date].expense += transaction.amount;
+            }
+
+            return acc;
         }, {});
+
+        //convert grouped to array and sort it by date
+        return Object.values(group).sort(
+          (a,b) => new Date(a.date) - new Date(b.date)
+        );
 
     }, [transactions, dateRange])
 
+    const totals = useMemo(()=> {
+      return filteredData.reduce(
+        (acc,day) => ( {
+          income: acc.income + day.income,
+          expense: acc.expense + day.expense,
+        }),
+        {income: 0, expense:0}
+      );
+    }, [filteredData]);
+
+    
+
+    // console.log(filteredData);
+    
+
   return (
-    <div>
-        {/* <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, 
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-        <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-      </BarChart>
-    </ResponsiveContainer> */}
-    </div>
+
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-7">
+        <CardTitle className="text-base font-normal">
+          Transaction Overview
+        </CardTitle>
+        <Select defaultValue = {dateRange} onValueChange = {setDateRange}>
+          <SelectTrigger className="w-[140px]">
+            <SelectValue placeholder="Select Range" />
+          </SelectTrigger>
+          <SelectContent>
+            {Object.entries(DATE_RANGES).map(([key, {label}]) => {
+              return <SelectItem key={key} value ={key}>
+                {label}
+              </SelectItem>
+            })}
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+
+        <div className="flex justify-around text-sm mb-6">
+          <div className="text-center">
+            <p className='text-muted-foreground'>
+              Total Income
+            </p>
+            <p className='text-lg font-bold text-green-500'>
+              ₹{totals.income.toFixed(2)}
+            </p>
+          </div>
+
+          <div className="text-center">
+            <p className='text-muted-foreground'>
+              Total Expenses
+            </p>
+            <p className='text-lg font-bold text-red-500'>
+              ₹{totals.expense.toFixed(2)}
+            </p>
+          </div>
+
+          <div className="text-center">
+            <p className='text-muted-foreground'>
+              Net
+            </p>
+            <p className={`text-lg font-bold ${
+              totals.income - totals.expense >= 0
+              ? "text-green-500" 
+              : "text-red-500"
+            }`}
+            
+            >
+              ₹{(totals.income - totals.expense).toFixed(2)}
+            </p>
+          </div>
+        </div>
+
+        <div className="h-[300px]">
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                data={filteredData}
+                margin={{
+                  top: 10, 
+                  right: 10,
+                  left: 10,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                <XAxis dataKey="date" />
+                <YAxis 
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                  tickFormatter={(value) => `₹${value}`} 
+                />
+                <Tooltip formatter={(value) => [`₹${value}`, undefined]}/>
+                <Legend />
+                <Bar 
+                  dataKey="income" 
+                  fill="#22c55e" 
+                  radius={[4,4,0,0]}
+                />
+
+                <Bar 
+                  dataKey="expense" 
+                  fill="#ef4444" 
+                  radius={[4,4,0,0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>  
+    </div>    
+    </CardContent>
+     
+    </Card>
+
+    
+  
   )
 }
 
